bea6c72781af6d258ebc93898c786c0d9e43f0dc
talentL <947130175@qq.com>
2017-10-12 21:58:17 +0800

    change 3 files on B2

--- Lab1/src/txt2Graph.java
+++ Lab1/src/txt2Graph.java
@@ -32,7 +32,7 @@ public class txt2Graph {
 	//show frame
 	static class translatorFrame extends JFrame{
 		public translatorFrame(){
-			setTitle("实验一：文本有向图");
+			setTitle("文本有向图");
 			setSize(DEFAULT_WIDTH,DEFAULT_HEIGHT);
 			controlPanel control = new controlPanel();
 			add(control,BorderLayout.SOUTH);
